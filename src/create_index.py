47f018a149a9dcfc1e3244d9f0647aa694e6e2f0
PinkFrojdSenjak <pavle.padjin@gmail.com>
2024-11-18 21:10:52 +0000

    adding ability to cache load index if already created

--- src/create_index.py
+++ src/create_index.py
@@ -2,6 +2,7 @@ import os
 from llama_index.core import VectorStoreIndex, SimpleDirectoryReader, Settings
 from llama_index.llms.groq import Groq
 from llama_index.embeddings.huggingface import HuggingFaceEmbedding
+from llama_index.core import load_index_from_storage, StorageContext
 from llama_index.llms.huggingface_api import HuggingFaceInferenceAPI
 from transformers import AutoTokenizer
 from typing import List
@@ -18,16 +19,6 @@ def create_index(input_dir: str = None, input_files: List[str] = None, save_dir=
         Directory to save the index.
     """
 
-    if not input_dir and not input_files:
-        raise ValueError("Either input_dir or input_files must be provided.")
-    
-    if input_dir and input_files:
-        input_dir = None
-        raise Warning("Both input_dir and input_files are provided. input_files will be used.")
-
-    # Load documents
-    documents = SimpleDirectoryReader(input_dir=input_dir, input_files=input_files).load_data()
-
     llm = None
     if model_provider == 'groq':
         groq_key = os.environ['GROQ_KEY']
@@ -39,6 +30,23 @@ def create_index(input_dir: str = None, input_files: List[str] = None, save_dir=
 
     Settings.llm = llm
     Settings.embed_model = embed_model
+
+    if os.path.exists(save_dir) and os.path.exists(os.path.join(save_dir, "index_store.json")):
+        print('Index already exists. Loading...')
+
+        storage_context = StorageContext.from_defaults(persist_dir=save_dir)
+        index = load_index_from_storage(storage_context)
+        return index
+
+    if not input_dir and not input_files:
+        raise ValueError("Either input_dir or input_files must be provided.")
+    
+    if input_dir and input_files:
+        input_dir = None
+        raise Warning("Both input_dir and input_files are provided. input_files will be used.")
+
+    # Load documents
+    documents = SimpleDirectoryReader(input_dir=input_dir, input_files=input_files).load_data()
 
     # Create index
     index = VectorStoreIndex.from_documents(
